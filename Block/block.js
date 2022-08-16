9519eeaa6ea7bed6b0f55b757f045984d5157221
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-08-16 08:17:16 +0600

    Block

---
+++ Block/block.js
@@ -0,0 +1,14 @@
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+
+}
+
+console.log(a)   // Print the value 
+console.log(b)  // Reference Error 
+console.log(c) // Reference Error 
