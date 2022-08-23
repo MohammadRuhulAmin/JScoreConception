1e89a677eb70d37a91a7e10a9bf8ba9924411049
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-08-23 10:40:07 +0600

    Objects & more

--- Basic Conception/Object/Object.js
+++ Basic Conception/Object/Object.js
@@ -10,4 +10,22 @@ const person = {
 }
 
 
-console.log(person.fullName());
+// console.log(person.fullName());
+
+// JavaScript objects cannot be compared.
+
+
+// objects value can be compared ! 
+function checkObject(){
+    let obj_1 = {
+        name:"Ruhul"
+    }
+    let obj_2 = {
+        name: "Ruhul"
+    }
+    if(obj_1 === obj_2)console.log("Objects  matches !!");
+    else {
+        console.log("Objects cannot be compared !");
+    }
+}
+checkObject();
