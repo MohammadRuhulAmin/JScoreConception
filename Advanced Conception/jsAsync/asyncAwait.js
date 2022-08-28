bd20d90d5959872919b2e1358fbe02732204d7c0
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2022-08-28 06:41:52 +0600

    async await try catch

--- Advanced Conception/jsAsync/asyncAwait.js
+++ Advanced Conception/jsAsync/asyncAwait.js
@@ -1,2 +1,77 @@
 
 
+
+const taskone = ()=>{
+    return new Promise((resolve,reject)=>{
+        resolve("Task One Completed");
+
+    })
+}
+
+const taskTwo = ()=>{
+    const isTaskOneDone = true;
+    return new Promise((resolve,reject)=>{
+        if(isTaskOneDone){
+            resolve("Task Two is Completed");
+        }else{
+            reject("Task Two is not Completed");
+        }
+    })
+}
+
+const taskThree = ()=>{
+    return new Promise((resolve,reject)=>{
+        resolve("Task Three is Done")
+    })
+}
+
+
+// taskone()
+//     .then((resolve)=>{
+//         console.log(resolve)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+//     .then((taskTwo))
+//     .then((resolve)=>{
+//         console.log(resolve)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+//     .then((taskThree))
+//     .then((resolve)=>{
+//         console.log(resolve)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+
+async function callAlltheTasks(){
+    try{
+        const t1 = await taskone();
+        console.log(t1);
+        const t2 = await taskTwo();
+        console.log(t2);
+        const t3 = await taskThree();
+        console.log(t3);
+    }catch(error){
+        console.log(error)
+    }
+}
+
+callAlltheTasks();
+
+const allTask = async ()=>{
+    const t1 = await taskone();
+    console.log(t1);
+    const t2 = await taskTwo();
+    console.log(t2);
+    const t3 = await taskThree();
+    console.log(t3);
+}
+
+console.log(allTask())
+
+
